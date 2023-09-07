831ea5a7ce01bf01d52caeeedd0ca75dc631b4f4
Megan Buglio <megan.buglio@gmail.com>
2023-09-07 14:40:34 -0500

    updated files

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ input.addEventListener('keyup', searchHandler);
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+document.addEventListener('DOMContentLoaded',function(){
+	input.value = '';
+})
+
 function search(input) {
 	let results = [];
 	const userInput = input.toLowerCase();
@@ -21,7 +25,7 @@ function search(input) {
 
 
 function searchHandler(e) {
-	const userInput = input.value;
+	const userInput = e.target.value;
 	const results = search(userInput);
 	showSuggestions(results,userInput);
 }
